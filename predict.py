40edfba39ee539138f3ec97dd0d95de2f16e8092
Yizhar Goldstein <izhargo@gmail.com>
2024-07-12 19:03:52 +0300

    irmi's project

--- predict.py
+++ predict.py
@@ -1,7 +1,10 @@
 import torch
+from torch.nn import ReLU
 
 
 if __name__ == '__main__':
-    tensor1 = torch.randint(0, 2, (4, 4))
+    tensor1 = torch.randint(-2, 2, (4, 4))
     tensor2 = torch.randint(0, 2, (4, 4))
-    print((tensor2*))
+    m = ReLU()
+    print(tensor1)
+    print(m(tensor1))
